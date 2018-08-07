b25b1706f58f6e3f27227843b3cc091a545d7994
maxime.jaouen <maxime.jaouen@e-doceo.net>
2018-08-07 17:07:23 +0200

    Update navigation

--- App.js
+++ App.js
@@ -1,8 +1,7 @@
 import React from 'react';
-import {StyleSheet, Text, View} from 'react-native';
-import {Header} from 'react-native-elements';
-//import Icon from 'react-native-vector-icons/FontAwesome';
-import { createStackNavigator } from 'react-navigation';
+import {StyleSheet, Text, View, Button} from 'react-native';
+import {Header, Icon} from 'react-native-elements';
+import {createStackNavigator} from 'react-navigation';
 
 import AYM from './src/component/AYM/AYM';
 
@@ -11,9 +10,27 @@ class HomeScreen extends React.Component {
         return (
             <View style={{flex: 1}}>
                 <Header
-                    leftComponent={{icon: 'menu', color: '#fff'}}
+                    leftComponent={
+                        <Icon
+                            name='home'
+                            type='font-awesome'
+                            color='#fff'
+                        />
+                    }
                     centerComponent={{text: 'AYM', style: {color: '#fff'}}}
-                    rightComponent={{icon: 'home', color: '#fff'}}
+                    rightComponent={
+                        <Icon
+                            name='angle-right'
+                            type='font-awesome'
+                            color='#fff'
+                            onPress={() => {
+                                this.props.navigation.navigate('Details', {
+                                    itemId: 86,
+                                    otherParam: 'anything you want here',
+                                });
+                            }}
+                        />
+                    }
                 />
                 <AYM server=""
                      meeting_id=""
@@ -28,8 +45,34 @@ class HomeScreen extends React.Component {
 class DetailsScreen extends React.Component {
     render() {
         return (
-            <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-                <Text>Details Screen</Text>
+            <View style={{flex: 1}}>
+                <Header
+                    centerComponent={{text: 'AYM', style: {color: '#fff'}}}
+                    leftComponent={
+                        <Icon
+                            name='angle-left'
+                            type='font-awesome'
+                            color='#fff'
+                            onPress={() => {
+                                this.props.navigation.navigate('Home', {
+                                    itemId: 86,
+                                    otherParam: 'anything you want here',
+                                });
+                            }}
+                        />
+                    }
+                />
+                <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+                    <Text>Details Screen</Text>
+                    <Button
+                        title="Go to Home"
+                        onPress={() => this.props.navigation.navigate('Home')}
+                    />
+                    <Button
+                        title="Go back"
+                        onPress={() => this.props.navigation.goBack()}
+                    />
+                </View>
             </View>
         );
     }
